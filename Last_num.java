397d6678d80fafabb1f907b3598c952f10c4b7e6
madhesh4321 <231901029@rajalakshmi.edu.in>
2024-08-06 08:59:08 +0530

    Add files via upload

---
+++ Last_num.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Last_num{
+public static void main(String [] args){
+Scanner number= new Scanner(System.in);
+System.out.println("Enter a number:");
+int num=number.nextInt();
+int a=num%10;
+System.out.println(a);
+}
+}
