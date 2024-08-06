fe6b307c71451e98f7134ed4da9092e246866ac8
madhesh4321 <231901029@rajalakshmi.edu.in>
2024-08-06 08:26:59 +0530

    CheckOddEven.java

--- CheckOddEven.java
+++ CheckOddEven.java
@@ -4,11 +4,11 @@ public static void main(String[] args) {
 Scanner num = new Scanner(System.in);
 System.out.print("Enter a number: ");
 int number = num.nextInt();
-        if (number % 2 != 0) {
-            System.out.println(2);
-        } else {
-            System.out.println(1);
-        }
-        num.close();
-    }
+if (number % 2 != 0) {
+System.out.println(2);
+} else {
+System.out.println(1);
+}
+num.close();
+}
 }
