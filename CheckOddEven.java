f12773b86bfb3a82d059118267ada75c64c40ea7
madhesh4321 <231901029@rajalakshmi.edu.in>
2024-08-06 08:19:55 +0530

    Add files via upload

---
+++ CheckOddEven.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class CheckOddEven {
+public static void main(String[] args) {
+Scanner num = new Scanner(System.in);
+System.out.print("Enter a number: ");
+int number = num.nextInt();
+        if (number % 2 != 0) {
+            System.out.println(2);
+        } else {
+            System.out.println(1);
+        }
+        num.close();
+    }
+}
